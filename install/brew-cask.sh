df4e6ca49005dce88ca462412cd078fd2f85ec4a
Phil Harle <philharle@gmail.com>
2017-10-06 11:14:21 +0100

    Additional dotfiles and brew scripts

---
+++ install/brew-cask.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Caskroom
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+# Install packages
+apps=(
+	spectacle
+	iterm2
+	google-chrome
+	spotify
+)
+
+brew cask install "${apps[@]}"
+
---
+++ install/brew.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Installs Homebrew and some of the common dependencies needed/desired for software development
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure were using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade --all
+
+# Install the Homebrew packages I use on a day-to-day basis.
+
+apps=(
+	git
+	wget
+)
+
+brew install "${apps[@]}"
+
+# Remove outdated versions from the cellar
+brew cleanup

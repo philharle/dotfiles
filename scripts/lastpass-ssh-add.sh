64b12775700bcb09ed2f212b5bb57379f4a0cb86
Phil Harle <philharle@users.noreply.github.com>
2018-11-13 11:02:14 +0000

    Create lastpass-ssh-add.sh

---
+++ scripts/lastpass-ssh-add.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Author: https://github.com/bellstrand/lastpass-ssh-add
+
+for id in `lpass ls | grep '\/.\+ \[id' | sed -n 's/^.*\[id: \([0-9]*\)\].*$/\1/p'`; do
+	if [ "`lpass show $id | grep 'Private Key'`" ]; then
+		name=`lpass show $id --name`
+		key=`lpass show $id --field='Private Key'`
+
+		if [ "$key" ] && [ ! -f ~/.ssh/$name ]; then
+			echo "Adding ssh-key: id = $id, name = $name"
+			echo "$key" > ~/.ssh/$name
+			chmod 400 ~/.ssh/$name
+		fi
+	fi
+done
